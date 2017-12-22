b3df6396291a1badd199325b8eabacbf62426c09
Rob Johnson <rob.johnson@libertymutual.com>
2017-12-22 09:41:04 -0500

    initial version of Jeopardy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
 import {JeopardyService } from './jeopardy.service';
 
 @Component({
@@ -10,6 +10,10 @@ export class AppComponent implements OnInit {
   title = 'app';
 
   questionInfo;
+  
+  playerAnswer: string; 
+  playerTotal = 0;
+  showAnswer;
 
   constructor(private jeopardyService: JeopardyService){}
 
@@ -18,9 +22,19 @@ export class AppComponent implements OnInit {
       .subscribe(
         questionInfo => {
           this.questionInfo = questionInfo[0];
+          this.showAnswer = false;
         }
       )
   }
+
+  clickSubmit() {
+    if(this.playerAnswer == this.questionInfo.answer) {
+      this.playerTotal = this.playerTotal + this.questionInfo.value
+  } 
+  this.showAnswer = true;
+  this.playerAnswer = ""
+  return this.playerTotal
+}
 
   ngOnInit(){
     this.getDataFromService()
